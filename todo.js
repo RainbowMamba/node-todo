278931822a83bd86b4ae021d261f5263d47bd138
jeffKim <kwangho5538@gmail.com>
2026-03-04 10:43:23 +0900

    refactor: extract todo operations into separate functions. Challenges: None

--- todo.js
+++ todo.js
@@ -15,16 +15,15 @@ function saveTodos(todos) {
   fs.writeFileSync(TODOS_FILE, JSON.stringify(todos, null, 2));
 }
 
-const [, , command, ...args] = process.argv;
-
-if (command === "add") {
-  const content = args.join(" ");
+function addTodo(content) {
   const todos = loadTodos();
   const newId = todos.length > 0 ? Math.max(...todos.map((t) => t.id)) + 1 : 1;
   todos.push({ id: newId, content, done: false });
   saveTodos(todos);
   console.log(`Todo가 추가되었습니다: ${content}`);
-} else if (command === "list") {
+}
+
+function listTodos() {
   const todos = loadTodos();
   if (todos.length === 0) {
     console.log("Todo가 없습니다.");
@@ -34,8 +33,9 @@ if (command === "add") {
       console.log(`[${check}] ${index + 1}. ${todo.content}`);
     });
   }
-} else if (command === "done") {
-  const id = parseInt(args[0]);
+}
+
+function doneTodo(id) {
   const todos = loadTodos();
   const todo = todos.find((t) => t.id === id);
   if (!todo) {
@@ -45,9 +45,9 @@ if (command === "add") {
     saveTodos(todos);
     console.log(`ID ${id}번 항목이 완료되었습니다.`);
   }
-} else if (command === "update") {
-  const id = parseInt(args[0]);
-  const newContent = args.slice(1).join(" ");
+}
+
+function updateTodo(id, newContent) {
   const todos = loadTodos();
   const todo = todos.find((t) => t.id === id);
   if (!todo) {
@@ -59,8 +59,9 @@ if (command === "add") {
     saveTodos(todos);
     console.log(`ID ${id}번 항목이 수정되었습니다: ${newContent}`);
   }
-} else if (command === "delete") {
-  const id = parseInt(args[0]);
+}
+
+function deleteTodo(id) {
   const todos = loadTodos();
   const index = todos.findIndex((t) => t.id === id);
   if (index === -1) {
@@ -71,4 +72,18 @@ if (command === "add") {
     saveTodos(todos);
     console.log(`ID ${id}번 항목이 삭제되었습니다: ${removed.content}`);
   }
+}
+
+const [, , command, ...args] = process.argv;
+
+if (command === "add") {
+  addTodo(args.join(" "));
+} else if (command === "list") {
+  listTodos();
+} else if (command === "done") {
+  doneTodo(parseInt(args[0]));
+} else if (command === "update") {
+  updateTodo(parseInt(args[0]), args.slice(1).join(" "));
+} else if (command === "delete") {
+  deleteTodo(parseInt(args[0]));
 }
